33b209134f6871a0289a38ec143e8313043e3697
802023 <136094955+devwebsoluctions@users.noreply.github.com>
2023-10-13 13:24:25 -0300

    listando

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
-export class TodoInputAddItensComponent {
+export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emmitItemTaskList = new EventEmitter(); //TODO: CRIA UM DECORADO PARA RECEBER VALORES DE SAIDA (Output) - E TRABALHA COM O EVENTMITTER PARA TRANMITIR O VALOR PARA QUALQUER COMPONENTE.
+
+  public addItemTaskList: string=""; //TODO: CRIA UMA VARIÁVEL PARA RECEBER O VALOR;
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  public submitItemTaskList(){
+    this.addItemTaskList = this.addItemTaskList.trim(); //TODO: O TRIM É RESPONSÁVEL POR REMOVER TODOS OS ESPAÇOS TANTO DA FRENTE QUEANTO DE TRÁS.
+
+    if(this.addItemTaskList){
+
+      this.emmitItemTaskList.emit(this.addItemTaskList);//TODO: EMITE O VALOR PARA DENTRO DA VARIÁVEL.
+
+      this.addItemTaskList = "";//TODO: LIMPA O VALOR DA VARIÁVEL.
+    }
+
+  }
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 //INTERFACE
 import { TaskList } from '../../model/task-list';
@@ -8,25 +8,41 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit{
+export class TodoListComponent implements DoCheck{
 
-  public taskList: Array<TaskList> = [
-    {task: 'Minha nova Task', checked: true},
-    {task: 'Minha nova Task 2', checked: false}
-  ];
+  //TODO: ARRAY QUE RECEBE OS VALORES.
+  public taskList: Array<TaskList> = [];
 
   constructor(){}
 
-  ngOnInit(): void {
-
+  //TODO: TODA A VEZ QUE OCORRER UMA MUDANÇA EM MEU COMPONENTE ESSE LIFE CIRCLE HOOK IRÁ CHAMAR E INFORMAR.
+  ngDoCheck(): void {
+    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked)); //TODO: ADICIONA O ITEM QUE ESTIVER CHECKADO NA ULTIMA POSIÇÃO DO ARRAY.
   }
 
+  //TODO: SETA OS VALORES DENTRO DO ARRAY ENTIANDO E TIPANDO.
+  public setEmitTaskList(event: string){
+    this.taskList.push({task: event, checked: false})
+  }
+
+  //TODO: DELETA UM ITEM POR VEZ DA LISTA.
   public deleteItemTaskList(event: number){
     this.taskList.splice(event, 1)
   };
 
+  //TODO: DELETA TODOS OS ITEMS DA LISTA.
   public deleteAllTaskList(){
     this.taskList = []
+  }
+
+  //TODO: VALIDA SE O USER QUER DEIXAR O INPUT EM BRANCO DANDO A OPÇÃO DE DELETAR CASO CONTRARIO ELE DEVER PREENCHER O INPUT.
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja deletar?");
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 
 }
